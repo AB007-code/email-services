41751c909f394e79380cd687f462764a741d1317
Abhilash <abhilash.vc888@gmail.com>
2025-08-13 00:59:32 +0530

    added

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ import express from "express";
 import cors from "cors";
 import routeHandler from "./routes/router.js";
 let port = process.env.PORT || 5000;
-let link = process.env.FRONTEND_LINK;
 const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: `${link}`, // Vite dev server
+    origin: process.env.FRONTEND_LINK, // Vite dev server
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
